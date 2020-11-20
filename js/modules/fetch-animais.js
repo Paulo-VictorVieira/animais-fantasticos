03ee6f6b74368661bdfdf115df90fd925167b397
Paulo Victor <pv-cunha@hotmail.com>
2020-11-20 17:58:17 -0300

    primeiro commit

---
+++ js/modules/fetch-animais.js
@@ -0,0 +1,25 @@
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function initFetchAnimais() {
+  function createAnimais(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+    div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+  async function fetchAnimais(url) {
+    try {
+      const animaisReponse = await fetch(url);
+      const animaisJson = await animaisReponse.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+      animaisJson.forEach((animal) => {
+        const divAnimal = createAnimais(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      initAnimaNumeros();
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+  fetchAnimais("./animaisapi.json");
+}
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+module.exports = {
+  entry: ['@babel/polyfill', './js/script.js'],
+  output: {
+    path: path.resolve(__dirname, './main.js'),
+    filename: 'main.js',
+  },
+};
